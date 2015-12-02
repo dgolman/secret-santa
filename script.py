53b14a216215e5436da8aa459742d6452e33e8bd
Daniel Golman <dgolman@mbp-dgolman.local>
2015-12-01 23:13:52 -0500

    removed credentials

--- script.py
+++ script.py
@@ -1,8 +1,8 @@
 from twilio.rest import TwilioRestClient 
  
 # put your own credentials here 
-ACCOUNT_SID = "AC624f172a6db0c9574c57ad67b424e02b" 
-AUTH_TOKEN = "dbbedf933cb071523f8ad0ba9f436bd7" 
+ACCOUNT_SID = "" 
+AUTH_TOKEN = "" 
  
 client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN) 
 
@@ -13,10 +13,10 @@ numbers = {
 	"Daniel": "+17039551026",
 	"Adam":   "+17039157591",
 	"Maia":   "+17038504482",
-	"Rhonda": "+17038196104", 
-	"Gary":   "+17038197172",
-	"Kerry":  "+17035851585"
+	"Kerry":  "+17035851585",
 }
+
+usedReceivers = {}
 
 result = {}
 
@@ -25,17 +25,19 @@ counter = 0
 while counter < len(numbers):
 	
 	[gifter, receiver] = random.sample(numbers, 2)
-	
-	if not gifter in result and gifter != receiver:
+
+	if not (gifter in result) and (gifter != receiver) and not (receiver in usedReceivers):
 		result[gifter] = receiver
+		usedReceivers[receiver] = 1
 		counter += 1
 
 
 for gifter, receiver in result.items():
-
-	client.messages.create(
-	    to=numbers[gifter], 
-	    from_="+15406803234", 
-	    body="Hi {0}, This is Moses, thou shall buy a gift for {1} by Sunday, Dec 6th. SHH!! Keep it a secret!".format(gifter, receiver), 
-	    media_url="http://goo.gl/V7LVmi", 
-	)
+	print [gifter, receiver]
+	# Uncomment to send text messages
+	# client.messages.create(
+	#     to=numbers[gifter], 
+	#     from_="+15406803234", 
+	#     body="Hi {0}, This is Moses, thou shall buy a gift for {1} by Sunday, Dec 6th. SHH!! Keep it a secret!".format(gifter, receiver), 
+	#     media_url="http://goo.gl/V7LVmi", 
+	# )
